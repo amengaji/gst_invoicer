205962054456b5ccbf2c71ed5eef888c08016e6c
Anuj <amengaji@gmail.com>
2025-12-05 13:56:58 +0530

    1.0.10 - Another attempt for clearing 400 bug

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = "my_super_secret_key_123"; // In production, use .env
 
+
 module.exports = (req, res, next) => {
+    console.log("Authorization header:", req.headers.authorization);
     const token = req.headers['authorization'];
     if (!token) return res.status(403).json({ error: "No token provided" });
 
